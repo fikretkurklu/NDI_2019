0a994158588ab28db47fddca1f331d56a33e576f
Geitner <geitnerteva@gmail.com>
2019-12-06 00:47:10 +0100

    homepage + page bourse

--- etuRoom/src/app/app.module.ts
+++ etuRoom/src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import {Routes, RouterModule} from "@angular/router";
+import { BourseComponent } from './bourse/bourse.component';
+
+const appRoutes: Routes = [
+  {path : "", component : HomepageComponent},
+  {path : "Bourse", component : BourseComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    BourseComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
