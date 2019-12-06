0e0f8d631ea5f7782de9be32b61078ecee6f6792
gregTres <gtrestour@gmail.com>
2019-12-06 03:57:10 +0100

    ajout internationnalisation

--- etuRoom/src/app/app.module.ts
+++ etuRoom/src/app/app.module.ts
@@ -1,16 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './core/components/navbar/navbar.component';
-import { HomepageComponent } from './homepage/homepage.component';
-import {Routes, RouterModule} from "@angular/router";
-import { BourseComponent } from './bourse/bourse.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NavbarComponent } from "./core/components/navbar/navbar.component";
+import { HomepageComponent } from "./homepage/homepage.component";
+import { Routes, RouterModule } from "@angular/router";
+import { BourseComponent } from "./bourse/bourse.component";
+import { TranslateModule, TranslateLoader } from "@ngx-translate/core";
+import { TranslateHttpLoader } from "@ngx-translate/http-loader";
+import { HttpClientModule, HttpClient } from "@angular/common/http";
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 const appRoutes: Routes = [
-  {path : "", component : HomepageComponent},
-  {path : "Bourse", component : BourseComponent}
+  { path: "", component: HomepageComponent },
+  { path: "Bourse", component: BourseComponent }
 ];
 
 @NgModule({
@@ -23,9 +30,17 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(appRoutes)
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes),
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
